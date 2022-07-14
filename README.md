9f4299b5125be134463b88705b0299a4998587ec
OhByeongHeon <hi1004@github.com>
2022-07-14 21:30:03 +0900

    clean code & update READE.md

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
-# 금호석유화학
+# KUMHO
 
-[DEMO](https://project1-two-rouge.vercel.app/)
+> KUMHO リニューアルサイト
+>
+> [DEMO](https://bit.ly/3Az3nqA)
+
+![image](https://raw.githubusercontent.com/hi1004/marugame-App/master/img/readme.jpg)
+
+</br>
+
+## 1. 製作期間 ＆ 参加人数
+
+- 2020 年 （約 3 週）
+- 参加人数：1 人
+
+</br>
+
+## 2. 使った技術
+
+#### `Front-End`
+
+- HTML5
+- CSS3
+- jquery 3
+- Slick
+- ScrollMagic
+
+<br>
+
+## 3. コア機能
+
+- Slick ライブラリーでスライド実装。
+- ScrollMagic ライブラリーを使い、scroll の時、画面転換効果実装。
+
+<details>
+	<summary><b>コードで見るコア機能</b></summary>
+  
+### 3.1. スライド
+
+- **Slick ライブラリー** :pushpin: [コード確認](https://github.com/hi1004/marugame-App/blob/master/js/index.js#L166-L329)
+
+### 3.2. ScrollMagic
+
+- **ScrollMagic ライブラリー** :pushpin: [コード確認](https://github.com/hi1004/marugame-App/blob/master/js/index.js#L333-L377)
+
+</details>
+
+<br>
+
+## 4. 成果 & 感じたこと
+
+- Slick ライブラリーについて理解できました。
+- ScrollMagic ライブラリーについて勉強になりました。
+- 言語が日本語である初プロジェクトです。
--- js/scroll_for.js
+++ js/scroll_for.js
@@ -1,114 +1,29 @@
 $(document).ready(function () {
+  var ww = $(window).width();
+  var wh = $(window).height();
 
-    var ww = $(window).width();
-    var wh = $(window).height();
+  $(window).resize(function () {
+    ww = $(window).width();
+    wh = $(window).height();
+  });
 
-    $(window).resize(function () {
-        ww = $(window).width();
-        wh = $(window).height();
-
-
-    });
-
-   /* function layout() {
-        var bg = ['red', 'pink', 'yellow', 'green'];
-        $('section').css({
-            backgroundColor: function (index) {
-                return bg[index];
-            },
-            width: ww,
-            height: wh,
-            //        lineHeight : wh,
-        });
+  $('.fullpage_section').on('mousewheel', function (event, delta) {
+    if (delta > 0) {
+      var prev = $(this).prev().offset().top;
+      $('html, body').stop().animate(
+        {
+          scrollTop: prev,
+        },
+        700
+      );
+    } else if (delta < 0) {
+      var next = $(this).next().offset().top;
+      $('html, body').stop().animate(
+        {
+          scrollTop: next,
+        },
+        700
+      );
     }
-    layout();
-
-    $('#but li').click(function () {
-        var ht = $(window).height();
-        var i = $(this).index();
-        var nowtop = i * ht;
-
-        $('body, html').stop().animate({
-            scrollTop: nowtop,
-        }, 1000);
-
-
-
-
-    });
-*/
-
-
-    //조건문활용    
-    /*$(window).scroll(function () {
-        var ht = $(window).height();
-        var scroll = $(window).scrollTop();
-
-        for (i = 0; i < 5; i++) {
-            if (scroll >= ht * i && scroll < ht * (i + 1)) {
-                $('#but li').removeClass();
-                $('#but li').eq(i).addClass('on');
-            };
-        }
-    });*/
-
-
-
-
-    //없어보이지만 구동되는 코드
-    //  $(window).scroll(function(){
-    //      var ht=$(window).height();
-    //      var scroll=$(window).scrollTop();
-    //      if(scroll>=ht*0 && scroll<ht*1){
-    //          $('#but li').removeClass();
-    //          $('#but li').eq(0).addClass('on');
-    //      }
-    //      else if(scroll>=ht*1 && scroll<ht*2){
-    //          $('#but li').removeClass();
-    //          $('#but li').eq(1).addClass('on');
-    //      }
-    //       else if(scroll>=ht*2 && scroll<ht*3){
-    //          $('#but li').removeClass();
-    //          $('#but li').eq(2).addClass('on');
-    //      }
-    //       else if(scroll<ht*4){
-    //          $('#but li').removeClass();
-    //          $('#but li').eq(3).addClass('on');
-    //      }
-    //      
-    //  }); 
-
-    //차이점
-    //$('선택자').on('이벤트', function(){}); 
-    //$('선택자').이벤트(function(){}); 
-
-
-   //event : 마우스 휠이 움직이는 이벤트 (마우스 휠이 움직일 때가 이벤트에 해당)
-    //delta : 마우스휠의 방향
-    //delta < 0 마우스휠을 위에서 아래로 움직일 때
-    //delta > 0 마우스휠을 아래에서 위로 움직일 때
-
-    $('.fullpage_section').on('mousewheel', function (event, delta) {
-        if (delta > 0) {
-            var prev = $(this).prev().offset().top;
-            $('html, body').stop().animate({
-                scrollTop: prev,
-                
-            }, 700);
-        } else if (delta < 0) {
-            var next = $(this).next().offset().top;
-            $('html, body').stop().animate({
-                scrollTop: next
-            }, 700);
-        }
-    });
-    
-    
-   
-
-
-
-
-
-
+  });
 }); //end
